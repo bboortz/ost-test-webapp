f719f3b5b9404c14243180bfb59e37b4bca1f6e3
Benjamin Boortz <benjamin.boortz@web.de>
2016-05-02 23:04:46 +0200

    first rss/atom server implementation /BB

--- msa-test-rss-feed/skeleton_webapp.py
+++ msa-test-rss-feed/skeleton_webapp.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+from os import sys, path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 import sys
 import os
+from restclient.restclient import RestClient, RestError
 import ssl
 import time
+import collections
 from datetime import datetime
 from urlparse import urljoin
 from flask import Flask, __version__, request
@@ -22,6 +26,26 @@ app.config['SECRET_KEY'] = 'top secret!'
 def make_external(url):
 	return urljoin(request.url_root, url)
 
+def print_item(feed, curr_status, curr_tests, url, from_timestamp, to_timestamp, first_print=False):
+	print "--- print"
+	from_date = datetime.utcfromtimestamp(from_timestamp)
+	to_date = datetime.utcfromtimestamp(to_timestamp)
+
+	if curr_status > 0:
+		if first_print:
+			title = "FAILED from %s to NOW" % (from_date)
+		else:
+			title = "FAILED from %s to %s" % (from_date, to_date)
+		text = "FAILED %d of %d tests" % (curr_status, curr_tests)
+	else:
+		if first_print:
+			title = "SUCCESS from %s to NOW" % (from_date)
+		else:
+			title = "SUCCESS from %s to %s" % (from_date, to_date)
+		text = "SUCCESS all of %s tests" % (curr_tests)
+
+	feed.add(title, unicode(text), content_type='html', author="ost-test-rss-feed", url=url, updated=to_date, published=from_date)
+
 
 @app.route('/alive')
 def alive():
@@ -33,10 +57,81 @@ def api():
 
 @app.route('/api/atom')
 def api_atom():
+	rc = RestClient(ssl_verify=False, timeout=0.1)
 	feed = AtomFeed('Recent Articles', feed_url=request.url, url=request.url_root)
-#	articles = Article.query.order_by(Article.pub_date.desc()).limit(15).all()
-#	for article in articles:
-	feed.add("title %s" % datetime.utcnow(), unicode("text %s" % datetime.utcnow() ), content_type='html', author="author", url=make_external("url"), updated=datetime.utcnow(), published=datetime.utcnow())
+
+        try:
+                result = rc.get("https://localhost:9091/api/info")
+		results = reversed( collections.OrderedDict( sorted( result.items() ) ) )
+
+		curr_id = None
+		curr_item = None
+		curr_timestamp = None
+		curr_summary = None
+		curr_status = None
+		curr_tests = None
+
+		next_id = None
+		next_item = None
+		next_timestamp = None
+		next_summary = None
+		next_status = None
+		next_tests = None
+
+		from_timestamp = None
+		to_timestamp = None
+		first_print = False
+
+		curr_id = results.next()
+		while True:
+			print "--> iteration start"
+			curr_item = result[curr_id]
+			curr_timestamp = curr_item['date']
+			curr_summary = curr_item['summary']
+			curr_status = curr_summary['failed']
+			if to_timestamp == None:
+				print "1 set to_timestamp %s" % curr_timestamp
+				to_timestamp = curr_timestamp
+			print "curr_id %s" % curr_id
+			try:
+				next_id = results.next()
+				next_item = result[next_id]
+				next_timestamp = next_item['date']
+				next_summary = next_item['summary']
+				next_status = next_summary['failed']
+				print "4 set from_timestamp %s" % curr_timestamp
+				from_timestamp = curr_item['date']
+				curr_tests = curr_summary['tests']
+				print "next_id %s" % next_id
+
+				if curr_status != next_status:
+					print_item(feed, curr_status, curr_tests, make_external("https://localhost:9091/api/info/%s" % curr_id), from_timestamp, to_timestamp, first_print)
+					to_timestamp = curr_timestamp
+					print "2 set to_timestamp %s" % curr_timestamp
+					first_print = False
+				curr_id = next_id
+
+			except:
+				print "--> exception"
+				print "3 set from_timestamp %s" % curr_timestamp
+				from_timestamp = curr_item['date']
+				curr_tests = curr_summary['tests']
+
+				print_item(feed, curr_status, curr_tests, make_external("https://localhost:9091/api/info/%s" % curr_id), from_timestamp, to_timestamp, first_print)
+				first_print = False
+
+				curr_id = next_id
+				break
+
+			print "<-- iteration end"
+
+		return feed.get_response()
+
+        except RestError as e:
+                print e.value
+        except Exception as e:
+                print e
+
 	return feed.get_response()
 
 @app.errorhandler(400)
